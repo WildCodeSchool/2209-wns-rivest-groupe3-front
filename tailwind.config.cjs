5658083d029a9b1d2c2491009489936bdb2d3e56
anas <anas@onicorn.dev>
2022-12-22 20:55:35 +0100

    :wrench:feat(tailwind config): ajout des couleurs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require('tailwindcss/defaultTheme');
+const defaultTheme = require('tailwindcss/defaultTheme')
 
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
@@ -16,4 +16,29 @@ module.exports = {
     },
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#092434',
+
+          secondary: '#D81E5B',
+
+          accent: '#37CDBE',
+
+          neutral: '#030C11',
+
+          'base-100': '#FFFFFF',
+
+          info: '#2B9EB3',
+
+          success: '#44AF69',
+
+          warning: '#FCAB10',
+
+          error: '#D81E5B',
+        },
+      },
+    ],
+  },
 }
