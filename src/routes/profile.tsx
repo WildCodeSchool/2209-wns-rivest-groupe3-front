2d4e0a02b47bdfc95b19c950194e27184e67c783
Thomas Biarneix <thomas.biarneix@voltyo.com>
2022-12-23 12:21:19 +0100

    feat(e03t01) getOneUser functionnal

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -14,93 +14,91 @@ const GET_ONE_USER = gql`
 `
 
 const Profil = () => {
-  const [user, setUser] = useState<IUser>({})
   const { loading, error, data } = useQuery(GET_ONE_USER, {
     variables: {
       nickname: 'test',
     },
   })
 
-  useEffect(() => {
-    setUser(data.getOneUser)
-  }, [data])
+  if (loading) return <p>Loading...</p>
+  if (error) return <p>Error</p>
+  if (data)
+    return (
+      <main className="min-h-screen w-full max-w-screen-2xl mx-auto my-8 flex flex-col items-center gap-8">
+        <h1 className="text-5xl font-bold text-center">Profile</h1>
+        <section className="card lg:card-side bg-base-100 shadow-xl">
+          <figure>
+            <img src="https://placeimg.com/400/400/arch" alt="Album" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">Pseudo</h2>
+            <p>{data.getOneUser.nickname}</p>
+            <h2 className="card-title">Presentation</h2>
+            <p>{data.getOneUser.description}</p>
+          </div>
+        </section>
+        <section className="py-16 flex flex-col items-center justify-center gap-16">
+          <h2 className="text-5xl font-bold text-center">Mes blogs</h2>
+          <article className="flex justify-center items-center gap-16">
+            <Card />
+          </article>
+        </section>
+        <section className="py-16 flex flex-col items-center justify-center gap-16">
+          <h2 className="text-5xl font-bold text-center">Mes articles</h2>
+          <article className="flex justify-center items-center gap-16">
+            <Card />
+            <Card />
+            <Card />
+          </article>
+          <a className="link link-hover text-xl">Voir plus</a>
+        </section>
+        <section className="card flex flex-col gap-8">
+          <h2 className="text-5xl font-bold text-center">Mes informations</h2>
 
-  return (
-    <main className="min-h-screen w-full max-w-screen-2xl mx-auto my-8 flex flex-col items-center gap-8">
-      <h1 className="text-5xl font-bold text-center">Profile</h1>
-      <section className="card lg:card-side bg-base-100 shadow-xl">
-        <figure>
-          <img src="https://placeimg.com/400/400/arch" alt="Album" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">Pseudo</h2>
-          <p>{user.nickname}</p>
-          <h2 className="card-title">Presentation</h2>
-          <p>{user.description}</p>
-        </div>
-      </section>
-      <section className="py-16 flex flex-col items-center justify-center gap-16">
-        <h2 className="text-5xl font-bold text-center">Mes blogs</h2>
-        <article className="flex justify-center items-center gap-16">
-          <Card />
-        </article>
-      </section>
-      <section className="py-16 flex flex-col items-center justify-center gap-16">
-        <h2 className="text-5xl font-bold text-center">Mes articles</h2>
-        <article className="flex justify-center items-center gap-16">
-          <Card />
-          <Card />
-          <Card />
-        </article>
-        <a className="link link-hover text-xl">Voir plus</a>
-      </section>
-      <section className="card flex flex-col gap-8">
-        <h2 className="text-5xl font-bold text-center">Mes informations</h2>
-
-        <label className="space-y-4">
-          <span className="card-title">Nom</span>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full w-full"
-          />
-        </label>
-        <label className="space-y-4">
-          <span className="card-title">Prénom</span>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full w-full"
-          />
-        </label>
-        <label className="space-y-4">
-          <span className="card-title">Ville</span>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full w-full"
-          />
-        </label>
-        <label className="space-y-4">
-          <span className="card-title">E-mail</span>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full w-full"
-          />
-        </label>
-        <label className="space-y-4">
-          <span className="card-title">Mot de passe</span>
-          <input
-            type="text"
-            placeholder="Type here"
-            className="input input-bordered w-full w-full"
-          />
-        </label>
-        <button className="btn btn-primary">Edit</button>
-      </section>
-    </main>
-  )
+          <label className="space-y-4">
+            <span className="card-title">Nom</span>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full w-full"
+            />
+          </label>
+          <label className="space-y-4">
+            <span className="card-title">Prénom</span>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full w-full"
+            />
+          </label>
+          <label className="space-y-4">
+            <span className="card-title">Ville</span>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full w-full"
+            />
+          </label>
+          <label className="space-y-4">
+            <span className="card-title">E-mail</span>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full w-full"
+            />
+          </label>
+          <label className="space-y-4">
+            <span className="card-title">Mot de passe</span>
+            <input
+              type="text"
+              placeholder="Type here"
+              className="input input-bordered w-full w-full"
+            />
+          </label>
+          <button className="btn btn-primary">Edit</button>
+        </section>
+      </main>
+    )
 }
 
 export default Profil
