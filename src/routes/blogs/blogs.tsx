ce013339ebc52b060432b002158a1bf94261a1a3
anas <anas@onicorn.dev>
2022-12-27 02:15:37 +0100

    :bento: feat(/blogs): ajout template 2

--- src/routes/blogs/blogs.tsx
+++ src/routes/blogs/blogs.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from 'react-router-dom'
 import BlogT1 from './BlogT1'
+import BlogT2 from './BlogT2'
 import ListingBlogs from './ListingBlogs'
 
 const Blogs = () => {
@@ -7,6 +8,7 @@ const Blogs = () => {
     <Routes>
         <Route element={<ListingBlogs />} path="/" />
         <Route element={<BlogT1 />} path="/:blogname" />
+        <Route element={<BlogT2 />} path="/:blogname/T2" />
     </Routes>
   )
 }
