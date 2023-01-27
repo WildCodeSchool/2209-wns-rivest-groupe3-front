4197c5d3a504e19e1162877b379c8a137d73c5a8
Antonia <antoniatch@zohomail.eu>
2023-01-27 14:32:22 +0100

    fix(e04-t07): delete coms and format with prettier

--- src/routes/createblog.tsx
+++ src/routes/createblog.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState, useEffect } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 import { gql, useMutation } from '@apollo/client'
 
 import ProgressionBar from '../components/inputs/ProgressionBar'
@@ -15,9 +15,9 @@ const CreateBlog = () => {
   const [name, setName] = useState<string | null>(null)
   const [description, setDescription] = useState<string | null>(null)
   const [template, setTemplate] = useState<number | null>(1)
-  const [nameAlert, setNameAlert] = useState<string | null>(null);
-  const [descriptionAlert, setDescriptionAlert] = useState<string | null>(null);
-  
+  const [nameAlert, setNameAlert] = useState<string | null>(null)
+  const [descriptionAlert, setDescriptionAlert] = useState<string | null>(null)
+
   const { user, setIsCreatingBlog } = useContext(UserContext)
 
   const navigate = useNavigate()
@@ -30,9 +30,8 @@ const CreateBlog = () => {
   }, [user])
 
   const validateBlog = () => {
-
-    let nameInput = {name};
-    let descriptionInput = {description};
+    let nameInput = { name }
+    let descriptionInput = { description }
 
     nameValidation
       .validate(nameInput)
@@ -59,9 +58,6 @@ const CreateBlog = () => {
           setDescriptionAlert(message)
         })
       })
-    
-
-
   }
 
   const nextStep = () => {
@@ -81,12 +77,15 @@ const CreateBlog = () => {
   }
 
   const templateAlert = () => {
-    // TODO Toaster d'alerte 'Tu n'as pas choisi de template'
     alert("Tu n'as pas choisi de template")
   }
 
   const CREATE_BLOG = gql`
-    mutation Mutation($description: String!, $name: String!, $template: Float!) {
+    mutation Mutation(
+      $description: String!
+      $name: String!
+      $template: Float!
+    ) {
       createBlog(description: $description, name: $name, template: $template) {
         name
         id
@@ -107,65 +106,66 @@ const CreateBlog = () => {
       .then((res) => {
         const blogName = res.data.createBlog.name
         const userName = user?.nickname
-        // TODO : concaténation pour définir l'URL de la page du blog ?
 
         alert(
           `Félicitations ${userName}, tu viens de créer ton blog ${blogName} !`
         )
 
         navigate(`/blogs/${blogName}`)
-        
       })
       .catch((err) => {
         console.log(err)
       })
   }
 
-
   return (
     <section className="min-h-screen max-w-screen flex justify-center items-center flex-col m-8">
-      <article className='flex justify-center items-center w-5/6 flex-col'>
+      <article className="flex justify-center items-center w-5/6 flex-col">
+        <ProgressionBar step={step}></ProgressionBar>
 
-      <ProgressionBar step={step}></ProgressionBar>
+        {step === 'first' ? <Register></Register> : null}
 
-      {step === 'first' ? <Register></Register> : null}
-      {/* 
-      Remplacer par le composant de login ou de register, et non la page entière */}
-      {step === 'second' ? (
-        <StepTwo setName={setName} setDescription={setDescription} nameAlert={nameAlert} descriptionAlert={descriptionAlert}></StepTwo>
-      ) : null}
-      {step === 'third' ? (
-        <StepThree setTemplate={setTemplate} template={template}></StepThree>
-      ) : null}
-
-      <div className="group flex w-full justify-end">
-        {step === 'first' ? null : (
-          <button className="btn btn-ghost" onClick={() => previousStep()}>
-            Précédent
-          </button>
-        )}
         {step === 'second' ? (
-          <button
-          className="btn"
-          onClick={() => {
-            step === 'second' ? validateBlog() : nextStep()
-          }}
-          >
-            Etape suivante
-          </button>
+          <StepTwo
+            setName={setName}
+            setDescription={setDescription}
+            nameAlert={nameAlert}
+            descriptionAlert={descriptionAlert}
+          ></StepTwo>
         ) : null}
+        
         {step === 'third' ? (
-          <div
-          className="btn"
-          onClick={() => {
-            template ? createNewBlog() : templateAlert()
-          }}
-          >
-            Voir mon blog
-          </div>
+          <StepThree setTemplate={setTemplate} template={template}></StepThree>
         ) : null}
-      </div>
-        </article>
+
+        <div className="group flex w-full justify-end">
+          {step === 'first' ? null : (
+            <button className="btn btn-ghost" onClick={() => previousStep()}>
+              Précédent
+            </button>
+          )}
+          {step === 'second' ? (
+            <button
+              className="btn"
+              onClick={() => {
+                step === 'second' ? validateBlog() : nextStep()
+              }}
+            >
+              Etape suivante
+            </button>
+          ) : null}
+          {step === 'third' ? (
+            <div
+              className="btn"
+              onClick={() => {
+                template ? createNewBlog() : templateAlert()
+              }}
+            >
+              Voir mon blog
+            </div>
+          ) : null}
+        </div>
+      </article>
     </section>
   )
 }
