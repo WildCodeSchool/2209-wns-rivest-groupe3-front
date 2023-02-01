a79282b92d7a8bc5851c856d202fd4ffbb9027d1
Holmes-EH <holmes.samuel@yahoo.fr>
2023-02-01 19:33:12 +0100

    :sparkles: feat(e06-t11): fetch blogs from backend

--- src/routes/blogs/blogs.tsx
+++ src/routes/blogs/blogs.tsx
@@ -1,13 +1,39 @@
-import { useState } from 'react'
+import { gql, useQuery } from '@apollo/client'
 import { Route, Routes } from 'react-router-dom'
 import BlogT1 from './BlogT1'
 import BlogT2 from './BlogT2'
 import EditDrag from './EditDrag'
 import ListingBlogs from './ListingBlogs'
+import { useContext } from 'react'
+import { NotificationContext } from '../../contexts/NotificationContext'
 
 const Blogs = () => {
-  const [isEditing, setIsEditing] = useState(false)
+  const { setMessage } = useContext(NotificationContext)
 
+  const GET_ALL_BLOGS = gql`
+    query GetAllBlogs {
+      getAllBlogs {
+        id
+        name
+        slug
+        description
+        template
+        createdAt
+        user {
+          id
+          nickname
+        }
+      }
+    }
+  `
+
+  const { loading, error, data } = useQuery(GET_ALL_BLOGS)
+
+  if (loading) return <>Loading...</>
+  if (error) {
+    setMessage({ text: error.message, type: 'error' })
+    return <></>
+  }
   return (
     <>
     {isEditing && <EditDrag setIsEditing={setIsEditing} />}
@@ -15,7 +41,7 @@ const Blogs = () => {
         <Route element={<ListingBlogs />} path="/" />
         <Route element={<BlogT2 setIsEditing={setIsEditing} />} path="/:blogname" />
         <Route element={<BlogT1 setIsEditing={setIsEditing} />} path="/:blogname/T1" />
-    </Routes></>
+    </Routes>
   )
 }
 
