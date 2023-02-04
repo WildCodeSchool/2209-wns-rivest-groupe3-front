e3300295080577c9a5f08b2415ea36399634a7d7
anas <anas@onicorn.dev>
2023-02-04 23:36:35 +0100

    :art:feat(epic0) rework of blog edition

--- src/queries/blogs.ts
+++ src/queries/blogs.ts
@@ -15,3 +15,64 @@ export const GET_ALL_BLOGS_FOR_DISCOVER = gql`
     }
   }
 `
+
+export const GET_ONE_BLOG = gql`
+  query GetBlog($slug: String!) {
+    getBlog(slug: $slug) {
+      id
+      name
+      description
+      template
+      slug
+      createdAt
+      editor: user {
+        id
+        avatar
+        nickname
+        city
+        description
+        blogs {
+          slug
+        }
+      }
+      articles {
+        id
+        slug
+        title
+        articleContent {
+          id
+          content {
+            time
+            version
+            blocks {
+              id
+              type
+              data {
+                text
+                level
+                style
+                items
+              }
+            }
+          }
+          version
+          current
+        }
+      }
+    }
+  }
+`
+
+export const CREATE_BLOG = gql`
+mutation Mutation(
+  $description: String!
+  $name: String!
+  $template: Float!
+) {
+  createBlog(description: $description, name: $name, template: $template) {
+    name
+    id
+    slug
+  }
+}
+`
